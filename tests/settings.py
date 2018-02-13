0957c0b22f8b66439b94cd9685baa915b27c133e
doganmeh <mehmet@edgle.com>
2018-02-13 13:37:17 -0600

    refactor tests

--- tests/settings.py
+++ tests/settings.py
@@ -19,7 +19,7 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'tests/test_app/db.sqlite3'),
     }
 }
 

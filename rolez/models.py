4e3a3303ff0c6779f99a43e7fd1ea918d8bbce5f
doganmeh <mehmet@edgle.com>
2018-04-07 21:04:42 -0500

    Role.save() save the permission name as well

--- rolez/models.py
+++ rolez/models.py
@@ -18,11 +18,15 @@ class AbstractRole(models.Model):
         return 'use_role_' + re.sub(r'([^\s\w]|_)+', '', self.name).replace(' ', '_') \
             .lower()
 
+    def perm_name(self):
+        return 'Can use role ' + self.name.lower()
+
     def save(self, **kwargs):
         if not hasattr(self, 'delegate'):
             ctype = ContentType.objects.get_for_model(self)  # takes obj or model
             self.delegate = Permission.objects.create(
                 content_type=ctype,
+                name=self.perm_name(),
                 codename=self.codename(),
             )
         super().save(**kwargs)
